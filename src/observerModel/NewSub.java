adb94a1ab4c66f5971d66834062cb1e3b6eebf94
andrew <270326408@qq.com>
2017-04-07 10:12:33 +0800

    观察着模式

---
+++ src/observerModel/NewSub.java
@@ -0,0 +1,14 @@
+package observerModel;
+
+import java.util.*;
+import java.util.Observer;
+
+/**
+ * Created by Andrew  on 2017/3/31.
+ */
+public class NewSub extends Observable implements Observer {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
